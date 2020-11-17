d1272d1c163482841771eaa7348cb9daf0244529
HoboCY <1830231903@qq.com>
2020-11-17 22:16:15 +0800

    fix view bug

--- host/Blog.MVC/wwwroot/js/editor.js
+++ host/Blog.MVC/wwwroot/js/editor.js
@@ -1,15 +1,1 @@
-﻿const E = window.wangEditor
-const editor = new E('#editor')
-editor.config.placeholder = 'Please enter the content here'
-editor.config.height = 500
-editor.config.uploadImgServer = "/File/Upload"
-editor.config.uploadImgMaxSize = 2 * 1024 * 1024
-editor.config.uploadImgMaxLength = 5
-
-editor.create()
-
-editor.txt.html()
-
-$('#add-post-button').click(function () {
-    console.log($editorContent.val())
-})
+﻿
