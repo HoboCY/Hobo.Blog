ad8918936f2b102af6e6c60c1043ec88cc8c00d9
HoboCY <1830231903@qq.com>
2020-11-30 13:12:29 +0800

    修改了StatusMessage的数据格式 新增了ProfileModel

---
+++ host/Blog.MVC/Models/StatusMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.MVC.Models
+{
+    public class StatusMessage
+    {
+        public StatusMessage(string statusMessageClass,string message)
+        {
+            StatusMessageClass = statusMessageClass;
+            Message = message;
+        }
+
+        public string StatusMessageClass { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ host/Blog.MVC/Models/User/Manage/ProfileModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.MVC.Models.User
+{
+    public class ProfileModel
+    {
+        [Display(Name = "用户名")]
+        [Required(ErrorMessage = "请输入{0}")]
+        [StringLength(30,ErrorMessage = "{0}必须至少{2}个字符，最多{1}个字符。", MinimumLength = 4)]
+        public string Username { get; set; }
+
+        [Phone(ErrorMessage = "{0}格式无效")]
+        [Display(Name = "手机号")]
+        [StringLength(11, ErrorMessage = "{0}必须至少{2}个字符，最多{1}个字符。", MinimumLength = 11)]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+    }
+}
