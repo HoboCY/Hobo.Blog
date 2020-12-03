8a6c254c76e169248d8e16c62e29c21619b4093f
HoboCY <1830231903@qq.com>
2020-12-03 22:28:46 +0800

    分类管理功能的viewmodel

---
+++ host/Blog.MVC/Models/Admin/CategoryEditViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.MVC.Models.Admin
+{
+    public class CategoryEditViewModel
+    {
+        [HiddenInput]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "请输入{0}")]
+        [Display(Name = "分类名称")]
+        [MaxLength(64,ErrorMessage = "{0}长度不超过{1}字符")]
+        public string CategoryName { get; set; }
+
+        public CategoryEditViewModel()
+        {
+            Id = Guid.Empty;
+        }
+    }
+}
---
+++ host/Blog.MVC/Models/Admin/CategoryManageViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Model;
+
+namespace Blog.MVC.Models.Admin
+{
+    public class CategoryManageViewModel
+    {
+        public CategoryEditViewModel CategoryEditViewModel { get; set; }
+
+        public IReadOnlyList<Category> Categories { get; set; }
+
+        public CategoryManageViewModel()
+        {
+            CategoryEditViewModel = new CategoryEditViewModel();
+            Categories = new List<Category>();
+        }
+    }
+}
