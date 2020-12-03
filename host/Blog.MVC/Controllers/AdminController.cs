94385334e84d27b92098c239ceca9e9daf6a869a
HoboCY <1830231903@qq.com>
2020-12-03 22:28:29 +0800

    完成Admin功能的分类管理

---
+++ host/Blog.MVC/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Blog.Data;
+using Blog.MVC.Models.Admin;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using X.PagedList;
+
+namespace Blog.MVC.Controllers
+{
+    [Authorize(Roles = "administrator")]
+    public class AdminController : Controller
+    {
+        private readonly BlogDbContext _context;
+
+        public AdminController(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("admin")]
+        public async Task<IActionResult> Category()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(new CategoryManageViewModel { Categories = categories });
+        }
+    }
+}
---
+++ host/Blog.MVC/Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Data;
+using Blog.Model;
+using Blog.MVC.Models.Admin;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Blog.MVC.Controllers
+{
+    [Authorize(Roles = "administrator")]
+    public class CategoryController : BlogController
+    {
+        private readonly BlogDbContext _context;
+
+        public CategoryController(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryEditViewModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest("参数错误");
+
+            var category = new Category
+            {
+                CategoryName = model.CategoryName,
+                NormalizedCategoryName = model.CategoryName.ToUpper(),
+                CreatorId = UserId,
+            };
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return Ok(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null) return NotFound("没有该分类");
+
+            var model = new CategoryEditViewModel
+            {
+                Id = category.Id,
+                CategoryName = category.NormalizedCategoryName
+            };
+            return Ok(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CategoryEditViewModel model)
+        {
+            if (!ModelState.IsValid) return BadRequest("参数错误");
+
+            var category = await _context.Categories.FindAsync(model.Id);
+            if (category != null)
+            {
+                category.CategoryName = model.CategoryName;
+                category.NormalizedCategoryName = model.CategoryName.ToUpper();
+                category.LastModificationTime = DateTime.UtcNow;
+                category.LastModifierId = UserId;
+
+                await _context.SaveChangesAsync();
+            }
+            return Ok(model);
+        }
+
+        [HttpDelete]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                return BadRequest("删除失败，参数错误");
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                category.IsDeleted = true;
+                category.DeleterId = UserId;
+                category.DeletionTime = DateTime.UtcNow;
+                await _context.SaveChangesAsync();
+            }
+            return Ok();
+        }
+    }
+}
