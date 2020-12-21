90cdcd0be8e0de88723b2e9bfba9f9fcb5b1204b
HoboCY <1830231903@qq.com>
2020-12-21 19:52:29 +0800

    引入axios 引入toastr 封装httpservice

---
+++ host/Blog.MVC/wwwroot/js/httpService.js
@@ -0,0 +1,39 @@
+﻿const xsrfFieldName = "XSRF-TOKEN-996BUG-INPUT";
+toastr.options = {
+    "positionClass": "toast-top-center",
+    "timeOut": "1500"
+}
+
+function httpDelete({ url, callback = function () { } }) {
+    request({ url: url, method: "DELETE", callback: callback });
+}
+
+function httpPost({ url, data = {}, callback = function () { } }) {
+    request({ url: url, data: data, callback: callback });
+}
+
+function httpGet(url) {
+    request({ url: url, method: "GET", callback: function () { } });
+}
+
+function request({ url, method = "POST", data = {}, callback }) {
+    const xsrfValue = $(`input[name=${xsrfFieldName}]`).val();
+    axios({
+        method: method,
+        url: url,
+        data: data,
+        xsrfCookieName: "XSRF-TOKEN-996BUG",
+        headers: { "X-XSRF-TOKEN": xsrfValue }
+    }).then(function (response) {
+        callback();
+        let message = "";
+        if (response.data !== null || response.data !== undefined || response.data !== "") {
+            message = response.data;
+        } else {
+            message = "操作成功";
+        }
+        toastr.success(message);
+    }).catch(function (error) {
+        toastr.error(error.response.data);
+    });
+}
---
+++ host/Blog.MVC/wwwroot/js/postManage.js
@@ -0,0 +1,17 @@
+﻿function deletePost(url, postid) {
+    httpDelete({
+        url: url,
+        callback: function () {
+            $(`#tr-${postid}`).hide();
+        }
+    });
+}
+
+function restorePost(url, postid) {
+    httpPost({
+        url: url,
+        callback: function () {
+            $(`#tr-${postid}`).hide();
+        }
+    });
+}
