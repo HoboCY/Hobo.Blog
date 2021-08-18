a94b66d054fe5de822767a223a6bff09d036aea8
HoboCY <1830231903@qq.com>
2021-08-18 17:53:52 +0800

    add custom not found exception

---
+++ src/Blog.Exceptions/BlogEntityNotFoundException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Exceptions
+{
+    public class BlogEntityNotFoundException : Exception
+    {
+        public Type EntityType { get; set; }
+
+        public string Parameter { get; set; }
+
+        public BlogEntityNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        public BlogEntityNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        public BlogEntityNotFoundException(Type entityType, string parameter, Exception innerException)
+            : base(
+                string.IsNullOrWhiteSpace(parameter)
+                    ? $"There is no such an entity given parameter. Entity type：{entityType.Name}"
+                    : $"There is no such an entity. Entity type：{entityType.Name}，parameter：{parameter}", innerException)
+        {
+            EntityType = entityType;
+            Parameter = parameter;
+        }
+
+        public BlogEntityNotFoundException(Type entityType, string parameter)
+            : this(entityType, parameter, null)
+        {
+        }
+    }
+}
