ccdc26990ed0f2630f036984dbd9ad8059f34742
hobocy <1830231903@qq.com>
2021-09-09 22:48:00 +0800

    rename project

--- src/Blog.ViewModels/Categories/CategoryViewModel.cs
+++ src/Blog.ViewModels/Categories/CategoryViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Blog.Model
+﻿namespace Blog.ViewModels.Categories
 {
     public class CategoryViewModel
     {
--- src/Blog.ViewModels/PostPreviewViewModel.cs
+++ src/Blog.ViewModels/PostPreviewViewModel.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Blog.ViewModels.Categories;
 
-namespace Blog.Model
+namespace Blog.ViewModels
 {
    public class PostPreviewViewModel
     {
