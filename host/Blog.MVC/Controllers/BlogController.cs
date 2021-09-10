bd43b226db38cdb38c4b9b0fcc644f3e2314a791
HoboCY <1830231903@qq.com>
2021-09-10 17:58:03 +0800

    add BlogController

---
+++ host/Blog.MVC/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Blog.MVC.Controllers
+{
+    public class BlogController : Controller
+    {
+        public Guid UserId()
+        {
+            var value= HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            return Guid.Parse(value);
+        }
+    }
+}
