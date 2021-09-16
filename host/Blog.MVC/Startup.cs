014a70cb2baf317ef7f8e683745574260f3c0ea3
HoboCY <1830231903@qq.com>
2021-09-16 17:48:15 +0800

    Add RazorRuntimeCompilation Package

--- host/Blog.MVC/Startup.cs
+++ host/Blog.MVC/Startup.cs
@@ -73,7 +73,7 @@ namespace Blog.MVC
             {
                 options.SuppressAsyncSuffixInActionNames = false;
                 //options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
-            });
+            }).AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
