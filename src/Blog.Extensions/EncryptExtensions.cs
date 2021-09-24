455fa2b553be8ef5c4c5e8ae9978e8b67d80d247
HoboCY <1830231903@qq.com>
2021-09-24 17:52:59 +0800

    add md5 encrypt extension

---
+++ src/Blog.Extensions/EncryptExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Blog.Extensions
+{
+    public static class EncryptExtensions
+    {
+        public static string Encrypt(this string input)
+        {
+            using var md5 = MD5.Create();
+            var data = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+            var encryptString = BitConverter.ToString(data);
+            return encryptString.Replace("-", "");
+        }
+    }
+}
