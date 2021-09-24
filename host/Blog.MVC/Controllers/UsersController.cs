eb1b8576b718057eb187b45a7db106ec3f02a8fc
HoboCY <1830231903@qq.com>
2021-09-24 17:54:31 +0800

    add UsersController

---
+++ host/Blog.MVC/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.MVC.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : BlogController
+    {
+        [HttpPost("login")]
+        public async Task LoginAsync()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "")
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                 new ClaimsPrincipal(claimsIdentity));
+        }
+
+        [HttpPost("logout")]
+        public async Task LogoutAsync()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "")
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
