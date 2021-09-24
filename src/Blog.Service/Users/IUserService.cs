efc622cef082a76ebee2a4c65f2c7cd6e9fd61b9
HoboCY <1830231903@qq.com>
2021-09-24 17:56:29 +0800

    add user service

---
+++ src/Blog.Service/Users/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Data.Entities;
+
+namespace Blog.Service.Users
+{
+    public interface IUserService
+    {
+        Task<AppUser> GetAsync(string email);
+    }
+}
---
+++ src/Blog.Service/Users/UserService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Blog.Data.Entities;
+using Blog.Data.Repositories;
+
+namespace Blog.Service.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly IRepository _repository;
+
+        public UserService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<AppUser> GetAsync(string email)
+        {
+           return await _repository.GetAsync<AppUser>(
+                "SELECT BIN_TO_UUID(id) AS Id,email AS Email,email_confirmed AS EmailConfirmed,password AS Password FROM `app_user` WHERE email = @Email", new { email });
+        }
+    }
+}
