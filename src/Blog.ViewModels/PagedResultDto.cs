e22d72ab160be318653fcd2654e6e0b59e12b5f3
HoboCY <1830231903@qq.com>
2021-09-29 17:58:29 +0800

    add PagedResultDto<T>

---
+++ src/Blog.ViewModels/PagedResultDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.ViewModels
+{
+   public class PagedResultDto<T>
+    {
+        public int Total { get; set; }
+
+        public List<T> Items { get; set; }
+
+        public PagedResultDto()
+        {
+            Items = new List<T>();
+        }
+    }
+}
