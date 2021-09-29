db3139c136e93861abacae5062bca3874a03ddd5
HoboCY <1830231903@qq.com>
2021-09-29 17:59:23 +0800

    add Required attribute

--- host/Blog.MVC/ViewModels/Categories/EditCategoryInputViewModel.cs
+++ host/Blog.MVC/ViewModels/Categories/EditCategoryInputViewModel.cs
@@ -9,6 +9,7 @@ namespace Blog.MVC.ViewModels.Categories
     public class EditCategoryInputViewModel
     {
         [MaxLength(100)]
+        [Required]
         public string CategoryName { get; set; }
     }
 }
