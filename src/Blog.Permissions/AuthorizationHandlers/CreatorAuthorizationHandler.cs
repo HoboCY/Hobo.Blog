ebccce0da7249e24940eb44c86ef5bef971b37fc
HoboCY <1830231903@qq.com>
2021-10-08 16:21:16 +0800

    add CreatorAuthorizationHandler

---
+++ src/Blog.Permissions/AuthorizationHandlers/CreatorAuthorizationHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Blog.Data;
+
+namespace Blog.Permissions.AuthorizationHandlers
+{
+    public class CreatorAuthorizationHandler<T> : AuthorizationHandler<OperationAuthorizationRequirement, T> where T : ICreator<string>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, OperationAuthorizationRequirement requirement,
+            T resource)
+        {
+
+            var id = context.User.FindFirst(ClaimTypes.NameIdentifier);
+            if (id?.Value == resource.CreatorId)
+            {
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
