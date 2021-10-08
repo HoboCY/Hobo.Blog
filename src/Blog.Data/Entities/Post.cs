41cd3fec15217e8979e71bdee29e3b3de9fd5d24
HoboCY <1830231903@qq.com>
2021-10-08 16:19:39 +0800

    remove CreatorId to interface

--- src/Blog.Data/Entities/Post.cs
+++ src/Blog.Data/Entities/Post.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Blog.Data.Entities
 {
-    public class Post
+    public class Post : ICreator<string>
     {
         public string Id { get; set; }
 
---
+++ src/Blog.Data/ICreator.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Data
+{
+    public interface ICreator<T>
+    {
+        [CanBeNull]
+        T CreatorId { get; set; }
+    }
+}
