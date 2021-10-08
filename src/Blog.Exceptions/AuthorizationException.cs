856d1226f7a96e93513f4518cc4d21a53e5d59b8
HoboCY <1830231903@qq.com>
2021-10-08 16:21:27 +0800

    add AuthorizationException

---
+++ src/Blog.Exceptions/AuthorizationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Exceptions
+{
+    public class AuthorizationException : Exception
+    {
+        public AuthorizationException(string message) : base(message)
+        {
+
+        }
+    }
+}
