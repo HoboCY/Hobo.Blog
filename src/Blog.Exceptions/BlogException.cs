9ed9030061a0847b3d52da336e3c6bf27fb1dcfc
HoboCY <1830231903@qq.com>
2021-10-09 16:42:50 +0800

    add new custom exception

---
+++ src/Blog.Exceptions/BlogException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Exceptions
+{
+    public class BlogException : Exception
+    {
+        public BlogException(string message) : base(message)
+        {
+
+        }
+
+        public BlogException(int statusCode, string message) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+
+        public BlogException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+
+        public BlogException(int statusCode, string message, Exception innerException) : base(message, innerException)
+        {
+            StatusCode = statusCode;
+        }
+
+        public int StatusCode { get; set; }
+    }
+}
