6a98cef7347c72459cf3a2c050bd1d76c12ce0fc
HoboCY <1830231903@qq.com>
2021-10-09 16:57:14 +0800

    add email check

--- src/Blog.ViewModels/Users/LoginViewModel.cs
+++ src/Blog.ViewModels/Users/LoginViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace Blog.ViewModels.Users
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels.Users
 {
     public class LoginViewModel
     {
+        [EmailAddress]
         public string Email { get; set; }
 
         public string Password { get; set; }
